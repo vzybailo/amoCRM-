80a2d010b8bd965d0e4d5a11e98731c9bc3c193d
Vitali Zybailo <30474269+vzybailo@users.noreply.github.com>
2019-02-19 19:56:27 +0200

    Add files via upload

--- server.php
+++ server.php
@@ -1,4 +1,37 @@
 <?php
-     echo: "flsfsdkflk";
+    $name = $_POST['user-name'];
+    $phone = $_POST['user-phone'];
+    $email = $_POST['user-email'];
+    
+   
 
+    $mail_message = '
+    <html>
+        <head>
+            <title>Заявка</title>
+        </head>
+        <body>
+            <h2>Заказ</h2>
+            <ul>
+                <li>Имя: ' . $name . '</li>
+                <li>Телефон: ' . $phone . '</li>
+                <li>Email: ' . $email . '</li>
+            </ul>
+        </body>
+    </html>    
+    ';
+
+    $headers = "From: Администратор сайта <crm4u@list.ru>\r\n".
+    "MIME-Version: 1.0" . "\r\n" .
+    "Content-type: text/html; charset=UTF-8" . "\r\n";
+    $mail = mail('vental@bk.ru', 'Заказ', $mail_message, $headers);
+    $data = [];
+    if ($mail) {
+        $data['status'] = "OK";
+        $data['mes'] = "Письмо успешно отправлено";
+    }else{
+        $data['status'] = "NO";
+        $data['mes'] = "На сервере произошла ошибка";
+    }
+    echo json_encode($data);
 ?>
