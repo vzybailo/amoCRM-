d70a9f7d756c5e0837ed02f112fcd7008039630e
Vitali Z <vitalizybailo@gmail.com>
2019-02-19 20:44:36 +0300

    19.02.2019

--- server.php
+++ server.php
@@ -1,9 +1,11 @@
 <?php
-     $name = $_POST['name'];
-     $phone = $_POST['phone'];
-     $email = $_POST['email'];
-     
-     $mail_message = '
+    $name = $_POST['user-name'];
+    $phone = $_POST['user-phone'];
+    $email = $_POST['user-email'];
+    
+   
+
+    $mail_message = '
     <html>
         <head>
             <title>Заявка</title>
@@ -19,4 +21,17 @@
     </html>    
     ';
 
+    $headers = "From: Администратор сайта <crm4u@list.ru>\r\n".
+    "MIME-Version: 1.0" . "\r\n" .
+    "Content-type: text/html; charset=UTF-8" . "\r\n";
+    $mail = mail('vental@bk.ru', 'Заказ', $mail_message, $headers);
+    $data = [];
+    if ($mail) {
+        $data['status'] = "OK";
+        $data['mes'] = "Письмо успешно отправлено";
+    }else{
+        $data['status'] = "NO";
+        $data['mes'] = "На сервере произошла ошибка";
+    }
+    echo json_encode($data);
 ?>
